481f5826c69bdf356849ddceb9fcd931699e6e2c
Sean Daly <sdaly@xosdigital.com>
2017-10-04 16:48:47 -0400

    add XMFTopology, not used yet

---
+++ XMFCaptureCPP/XMFTopology.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <windows.h>
+#include <mfidl.h>
+#include <mfapi.h>
+#include <mfreadwrite.h>
+#include <atlbase.h>
+
+class XMFAVSourceReader;
+class XMFSinkWriter;
+class XMFTopologyRep;
+class XMFTopology
+{
+public:
+	XMFTopology(XMFAVSourceReader* pXMFAVSourceReader, HWND hwndVideo, XMFSinkWriter* pXMFSinkWriter);
+	~XMFTopology();
+
+	HRESULT Stop();
+
+private:
+	XMFTopologyRep* m_pRep;
+};
